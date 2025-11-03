7ccdc0194374226075626bed41086564583acc63
Love Bellinder <love.bellinder@gmail.com>
2025-11-04 00:52:42 +0100

    Marking X and O is working.

--- Laxa1/MainWindow.xaml.cs
+++ Laxa1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,99 +23,30 @@ namespace Laxa1
             InitializeComponent();
         }
 
+
+
         public int player = 2;
+        public int turns = 0;
 
-        private void Ett_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (Ett.Content.ToString() == "X")
+            Button button = (Button)sender;
+            if (button.Content.ToString() == "")
             {
-                Ett.Content = "O";
-            }
-            else if (Ett.Content.ToString() == "O")
-            {
-                Ett.Content = "O";
-            }
-            else if (Ett.Content.ToString() == "")
-            {
-                Ett.Content = "X";
-            }
-        }
-        private void EttTva_Click(object sender, RoutedEventArgs e)
-        {
-            if (EttTva.Content.ToString() == "X")
-            {
-                EttTva.Content = "O";
-            }
-            else if (EttTva.Content.ToString() == "O")
-            {
-                EttTva.Content = "O";
-            }
-            else if (EttTva.Content.ToString() == "")
-            {
-                EttTva.Content = "X";
-            }
-        }
 
-        public void Text(object sender, RoutedEventArgs e)
-        {
-            EttTre.Content = "Love";
-        }
-
-        public void ForLoop(object sender, RoutedEventArgs e)
-        {
-            for (int i = 0; i < 1; i++)
-            {
-                if (player % 2 != 0)
+                if (player % 2 == 0)
                 {
-                    TvaEtt.Content = "X";
+                    button.Content = "X";
+                    player = player + 1;
+                    turns = turns + 1;
                 }
-                else
-                    TvaEtt.Content = "O";
+                else if (player % 2 != 0)
+                {
+                    button.Content = "O";
+                    player = player + 1;
+                    turns = turns + 1;
+                }
             }
-            player++;
-        }
-        public void ElseIf(object sender, RoutedEventArgs e)
-        {
-            if (player % 2 != 0)
-            {
-                TvaEtt.Content = "X";
-                player = player + 1;
-            }
-            else if (player % 2 == 0)
-            {
-                TvaEtt.Content = "O";
-                player = player + 1;
-            }
-        }
-
-        public void ColorSwap(object sender, RoutedEventArgs e)
-        {
-            Tre();
-        }
-        public void Tre()
-        {
-            TreTre.Background = Brushes.Red;
-        }
-
-        private void TreTre_MouseEnter(object sender, MouseEventArgs e)
-        {
-            TreTre.Background = Brushes.Purple;
-        }
-
-        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            TreTre.Background = Brushes.Blue;
-        }
-
-        private void Yellow(object sender, RoutedEventArgs e)
-        {
-            TvaTva.Background = Brushes.Yellow;
-            TvaTre.Background = Brushes.LemonChiffon;
-        }
-
-        private void TreTre_MouseEnter_1(object sender, MouseEventArgs e)
-        {
-
         }
     }
 }
