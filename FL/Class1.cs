7e9567544a0cf2e576a9ac7b6780f1f6d07103bf
Love Bellinder <love.bellinder@gmail.com>
2025-11-07 08:57:02 +0100

    More experiments with classes and parameters.

--- FL/Class1.cs
+++ FL/Class1.cs
@@ -17,9 +17,22 @@ namespace FL
     {
         public string model = "Volvo";
         public int speed = 120;
-        public void fullThrottle(TextBox textBox)
+        public void fullThrottle(TextBox textBox, string test)
         {
-            textBox.Text = "Full";
+            textBox.Text = test;
+        }
+
+    }
+
+    public class Robot
+    {
+        public static string Species = "Cyborg";
+
+        public string namn = "John";
+
+        public void sayHello(TextBox textBox)
+        {
+            textBox.Text = $"Hej, jag heter {namn}, jag är en {Species}";
         }
     }
 }
--- FL/MainWindow.xaml.cs
+++ FL/MainWindow.xaml.cs
@@ -18,11 +18,18 @@ namespace FL
         public MainWindow()
         {
             InitializeComponent();
+            robot.namn = "Love";
         }
-
-        public string full = "full";
+        public string full = "jag är en variabel";
         Person Love = new() { Name = "Love", Age = 25 };
         Car car = new();
+        Robot robot = new();
+
+
+        // static betyder tillgänglig för alla instanser.
+        static string firstName = "Erik";
+        static string lastName = "Öberg";
+        static string name = $"My full name is: {firstName} {lastName}";
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -31,13 +38,17 @@ namespace FL
 
         static public void Love_says_hi()
         {
-            MessageBox.Show("Love säger hej");
+            string hej = "hej";
+            MessageBox.Show($"Love säger {hej}");
+            MessageBox.Show($"{name}");
         }
 
         private void ok(object sender, RoutedEventArgs e)
         {
             //textBox.Text = Love.Name;
-            car.fullThrottle(textBox);
+            //car.fullThrottle(textBox, full);
+            robot.namn = "Love";
+            robot.sayHello(textBox);
 
         }
     }
